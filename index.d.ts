5995cfac2e2def102de400b9b52e7c690fa00010
Sam Elsamman <sam.elsamman@gmail.com>
2017-02-24 11:51:38 -0500

    Full Base Types

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,40 @@
 export class Supertype {
-    constructor ();
-    static fromJSON (json: string);
-    toJSONString();
+    constructor ()
+    static fromJSON (json: string)
+    static getFromPersistWithId(id?, cascade?, isTransient?, idMap?, isRefresh?, logger?)
+    static getFromPersistWithQuery(query, cascade?, start?, limit?, isTransient?, idMap?, options?, logger?)
+    static deleteFromPersistWithQuery (query, txn?, logger?)
+    static deleteByQuery(query, options)
+    static deleteFromPersistWithId (id, txn?, logger?)
+    static countFromPersistWithQuery(query?, logger?)
+    static fetchByQuery (query, options)
+    static deleteFromPersistWithId(id, txn?, logger?)
+    static getTableName(alias)
+    static getParentKey(prop, alias?)
+    static getPrimaryKey(alias?)
+    static getChildKey(prop, alias?)
+    static getTableName()
+    static getKnex()
+    static isKnex()
+    static knexParentJoin(targetTemplate, primaryAlias, targetAlias, joinKey)
+    static knexChildJoin(targetTemplate, primaryAlias, targetAlias, joinKey)
+    _id: Object;
+    __id__: String;
+
+    toJSONString()
+    setDirty()
+    cascadeSave(any)
+
+    persistSave (txn?, logger?)
+    persistTouch (txn?, logger?)
+    persistDelete (txn?, logger?)
+    setDirty (txn?, onlyIfChanged?, cascade?, logger?)
+    isStale ()
+    fetchProperty (prop, cascade?, queryOptions?, isTransient?, idMap?, logger?)
+    fetch(cascade, isTransient, idMap, logger)
+    fetchReferences(options)
+    refresh (logger?)
+    persist (options)
 }
 export function property(props?: Object);
 export function supertypeClass(target?: Function);
