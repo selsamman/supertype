1edb2622b50420bdb649d72071468049e04e8422
Nikhil Mashettiwar <nikhil@havenlife.com>
2017-03-13 16:15:51 -0400

    update jsdoc for extend function added null check for name property and added back test coverage npm script

--- index.js
+++ index.js
@@ -164,7 +164,7 @@ ObjectTemplate.setTemplateProperties = function setTemplateProperties(template, 
  * @returns {*} the object template
  */
 ObjectTemplate.create = function create(name, properties) {
-    if (typeof(name) != 'undefined' && name.name) {
+    if (name && name.name) {
         var props = name;
         name = props.name;
     }
@@ -205,7 +205,11 @@ ObjectTemplate.create = function create(name, properties) {
  * Extend and existing (parent template)
  *
  * @param {unknown} parentTemplate unknown
- * @param {unknown} name of the template
+ * @param {unknown} name the name of the template or an object with
+ *        name - the name of the class
+ *        toClient - whether the object is to be shipped to the client (with semotus)
+ *        toServer - whether the object is to be shipped to the server (with semotus)
+ *        isLocal - equivalent to setting toClient && toServer to false
  * @param {unknown} properties are the same as for create
  *
  * @returns {*} the object template
@@ -218,7 +222,7 @@ ObjectTemplate.extend = function extend(parentTemplate, name, properties) {
         throw new Error('incorrect parent template');
     }
 
-    if (typeof(name) != 'undefined' && name.name) {
+    if (name && name.name) {
         props = name;
         name = props.name;
     }
